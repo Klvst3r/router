230bd2629b096a2de9608f5b2c67b8d294fc1cad
Klvst3r <klvst3r@gmail.com>
2026-01-05 14:59:33 -0600

    9. Rutas anidads inicia

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,4 +1,4 @@
-components: { Users, },
+components: { Blog, }, components: { Users, },
 <template>
   <div v-if="$route.params.userId">
     <h1>User</h1>
@@ -8,11 +8,18 @@ components: { Users, },
     <h1>Users</h1>
     <p>Aqui podrás ver la lista de usuarios.</p>
   </div>
+  <div id="nav">
+    <nav>
+      <RouterLink to="/">Perfil</RouterLink>
+      <RouterLink to="/about">Cursos</RouterLink>
+    </nav>
+  </div>
 </template>
 
 <script>
 import Order from '@/views/Order.vue'
 import Users from '@/views/Users.vue'
+import Blog from '@/views/Blog.vue'
 
 export default {}
 </script>
